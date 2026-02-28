1405549ae2a7b1559153ad1199b26f98913fb24b
HarukiHirata <suwa3217@gmail.com>
2026-02-28 17:48:14 +0900

    20260228 プロジェクト作成

---
+++ src/main/java/com/keibaplus/webap/WebapApplication.java
@@ -0,0 +1,13 @@
+package com.keibaplus.webap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keibaplus/webap/WebapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keibaplus.webap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
